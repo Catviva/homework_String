52377da707e32e205ba2e34944a1e677806fa6ca
ekaterinasarapova <nkate05@mail.ru>
2022-05-12 23:08:31 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Exercise 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " +  middleName +" " + lastName;
+        {
+            System.out.println("ФИО сотрудника —  " + fullName);
+
+        // Exercise 2
+
+        fullName = fullName.toUpperCase();
+            System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+
+        // Exercise 3
+
+        fullName = "Иванов Семён Семёнович";
+            System.out.print("Данные ФИО сотрудника — " + fullName.replace('ё','е'));
+        }
+
     }
 }
